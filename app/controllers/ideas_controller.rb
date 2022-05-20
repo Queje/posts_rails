8cc77f6a93a79fa639ea9d84d91fad544adfb576
Queje <jeremyquerne@hotmail.fr>
2022-05-20 15:05:49 +0200

    adding tailwind and create a new post

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -7,4 +7,24 @@ class IdeasController < ApplicationController
     def show
         @idea = Idea.find(params[:id])
     end
+
+    def new
+        @idea = Idea.new
+    end
+    
+    def create
+        @idea = Idea.new(ideas_params)
+
+        if @idea.save
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def ideas_params
+        params.require(:idea).permit(:name, :description, :picture)
+    end
 end
